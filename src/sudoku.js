ab203a872c21670fecfbc20a3361006a1dad3a9f
Zungguzung <adron27@gmail.com>
2021-04-29 22:14:17 -0600

    Finally got sudoku solving algorithm to work

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,75 +1,92 @@
 
+const rowSize = 9;
 export function solvePuzzle(board)
 {
     let row;
     let col;
     [row,col] = findNextFree(board);
+    
     if(row == -1 || col == -1)
     {
         return true;
     }
+    let loc = row * rowSize + col;
+
     for(let i = 1 ; i <= 9; i++)
     {
-        if(!isNumUsed)
+        if(!isNumUsedRow(board,loc,i) && !isNumUsedCol(board,loc,i) && !isNumUsedSquare(board,loc,i))
         {
-            board[row * 9 + col] = i;
+            board[loc] = i;
+
             if(solvePuzzle(board))
             {
                 return true;
             }
-            board[row * 9 + col] = 0;
+            board[loc] = 0;
         }
     }
     return false;
 }
 
-export function isNumUsed(board, loc, num)
+export function isNumUsedRow(board, loc, num)
 {
-    const val = board[loc];
-    const row = Math.floor(loc / 9);
-    const col = loc % 9;
-
-    const squareRow = row - row % 3;
-    const squareCol = col - col % 3;
-
-    let result = false;
-
+    const row = Math.floor(loc / rowSize);
     //check the rows and columns
-    for(let i = 0; i < 9; i++)
+    for(let i = 0; i < rowSize; i++)
     {
-        let rowIndex = row + i;
-        let colIndex = i * 9 + col;
-        if((board[rowIndex] == num) || (board[colIndex] == num))
+        if(board[row * rowSize + i] == num)
         {
             return true;
         }
     }
 
-    //check the square
+    return false;
+}
+
+export function isNumUsedCol(board,loc,num)
+{
+    const col = loc % rowSize;
+    for(let i = 0; i < rowSize; i++)
+    {
+        if(board[i * rowSize + col] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+export function isNumUsedSquare(board, loc, num)
+{
+    const row = Math.floor(loc / rowSize);
+    const col = loc % rowSize;
+    const squareRow = row - row % 3;
+    const squareCol = col - col % 3;
+
     for(let i = 0; i < 3; i++)
     {
         for(let j = 0; j < 3; j++)
         {
             let rowIndex = squareRow + i;
-            let colIndex = j * 9 + squareCol
-            if(board[rowIndex + colIndex] == num)
+            let colIndex = squareCol + j;
+            if(board[rowIndex * rowSize + colIndex] == num)
             {
                 return true;
             }
         }
     }
-
     return false;
 }
 
 export function findNextFree(board)
 {
-    for(let i = 0; i < 9; i++)
+    for(let i = 0; i < rowSize; i++)
     {
-        for(let j = 0; j < 9; j++)
+        for(let j = 0; j < rowSize; j++)
         {
-            if(board[i * 9 + j] === 0)
+            if(board[i * rowSize + j] == 0)
             {
+
                 return [i,j];
             }
         }
--- src/sudoku.test.js
+++ src/sudoku.test.js
@@ -1,5 +1,4 @@
 import * as sudoku from './sudoku.js';
-import {solvePuzzle} from './sudoku.js';
 
 const testBoard1 = [ 
     3, 0, 6, 5, 0, 8, 4, 0, 0 ,
@@ -25,19 +24,128 @@ const testBoard1 = [
     7, 4, 5, 2, 8, 6, 3, 1, 9,
     ];
 
+    const noSol = [ 
+    0, 3, 6, 5, 0, 8, 4, 0, 0 ,
+    5, 2, 0, 0, 0, 0, 0, 0, 0 ,
+    0, 8, 7, 0, 0, 0, 0, 3, 1 ,
+    0, 0, 3, 0, 1, 0, 0, 8, 0 ,
+    9, 0, 0, 8, 6, 3, 0, 0, 5 ,
+    0, 5, 0, 0, 9, 0, 6, 0, 0 ,
+    1, 3, 0, 0, 0, 0, 2, 5, 0 ,
+    0, 0, 0, 0, 0, 0, 0, 7, 4 ,
+    0, 0, 5, 2, 0, 6, 3, 0, 0 
+    ];
+
+    const teest   =   [
+    3, 1, 6, 5, 2, 8, 4, 9, 7,
+    5, 2, 0, 0, 0, 0, 0, 0, 0,
+    0, 8, 7, 0, 0, 0, 0, 3, 1,
+    0, 0, 3, 0, 1, 0, 0, 8, 0,
+    9, 0, 0, 8, 6, 3, 0, 0, 5,
+    0, 5, 0, 0, 9, 0, 6, 0, 0,
+    1, 3, 0, 0, 0, 0, 2, 5, 0,
+    0, 0, 0, 0, 0, 0, 0, 7, 4,
+    0, 0, 5, 2, 0, 6, 3, 0, 0
+      ];
+
 describe("Test board solving", () => {
 
-    var input = testBoard1;
+    let input = testBoard1.slice();
+    let noSolIn = noSol.slice();
+    //sudoku.solvePuzzle(input);
+    //console.log(input);
     test('Test Board with solution 1', () => {
-        expect(solvePuzzle(input)).toBe(true);
+        expect(sudoku.solvePuzzle(input)).toBe(true);  
+        expect(input).toEqual(testBoard1Solved);
     });
 
-})
-
-describe("Test Number Used / Find next free", () => {
- //const solvePuzzle = require('./sudoku/solvePuzzle');
-    var input = testBoard1;
-    test('Test number used 1', () => {
-        expect(solvePuzzle(input, 0, 3)).toBe(true);
+    test('Test Board with solution 1', () => {
+        expect(sudoku.solvePuzzle(noSolIn)).toBe(false);  
+        expect(noSolIn).toEqual(noSol);
     });
 })
+
+describe("Test Number Used", () => {
+    let input = testBoard1.slice();
+    test('Test number used in place', () => {
+        expect(sudoku.isNumUsedRow(input, 0, 3)).toBe(true);
+    });
+
+    test('Test number used col', () => {
+        expect(sudoku.isNumUsedCol(input, 0, 1)).toBe(true);
+    });
+
+    test('Test number used col 2', () => {
+        expect(sudoku.isNumUsedCol(input, 8, 7)).toBe(false);
+    });
+
+    test('Test number used row', () => {
+        expect(sudoku.isNumUsedRow(input, 0, 4)).toBe(true);   
+    });
+
+    test('Test number used square 1', () => {
+        expect(sudoku.isNumUsedSquare(input, 0, 7)).toBe(true);
+    });
+
+    test('Test number used square 2', () => {
+        expect(sudoku.isNumUsedSquare(input, 40, 2)).toBe(false);
+    });
+
+    test('Test number used square 2', () => {
+        expect(sudoku.isNumUsedSquare(input, 80, 2)).toBe(true);
+    });
+
+    test('Test number not used 1', () => {
+        expect(sudoku.isNumUsedRow(input, 1, 1)).toBe(false);
+    });
+
+    test('Test number not used 2', () => {
+        expect(sudoku.isNumUsedCol(input, 4, 7)).toBe(false);
+    });
+
+    test('Test number not used in place', () => {
+        expect(sudoku.isNumUsedSquare(input, 7, 9)).toBe(false);
+    });
+})
+
+describe("Test Find Next Free", () => {
+    let input = testBoard1.slice();
+
+    let solvedPuzzle = testBoard1Solved.slice();
+    solvedPuzzle[80] = 0;
+
+    let input2 = input.slice();
+    input2[1] = 1;
+
+    let input3 = input2.slice();
+    input3[4] = 1;
+
+    let input4 = input3.slice();
+    input4[7] = 1;
+    input4[8] = 1;
+
+    test('Test 1', () => {
+        expect(sudoku.findNextFree(input)).toEqual([0,1]);
+    });
+    
+    test('Test 2', () => {
+        expect(sudoku.findNextFree(input2)).toEqual([0,4]);
+    });
+
+    test('Test 3', () => {
+        expect(sudoku.findNextFree(input3)).toEqual([0,7]);
+    });
+
+    test('Test 4', () => {
+        expect(sudoku.findNextFree(input4)).toEqual([1,2]);
+    });
+
+    test('Test full', () => {
+        expect(sudoku.findNextFree(testBoard1Solved)).toEqual([-1,-1]);
+    });
+
+    test('Test almost full', () => {
+        expect(sudoku.findNextFree(solvedPuzzle)).toEqual([8,8]);
+    });
+})
+
